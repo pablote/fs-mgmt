7826bae686ec768225142705f190a31dc93525fd
Pablo Compagni <pablocompagni@gmail.com>
2015-05-12 18:20:56 -0300

    using bower for js/css dependencies, includes deps bundled on app

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,27 @@ module.exports = function(grunt) {
             src: ['./app/**/*']
         },
 
+        uglify: {
+            options: {
+                sourceMap: true,
+                sourceMapIncludeSources: true
+            },
+            all: {
+                files: {
+                    'app/js/bundle.js': [
+                        'bower_components/jquery/dist/jquery.js',
+                        'bower_components/bootstrap/dist/js/bootstrap.js',
+                        'bower_components/jquery-growl/javascripts/jquery.growl.js',
+                        'bower_components/moment/moment.js',
+                        'bower_components/angular/angular.js',
+                        'bower_components/angular-sanitize/angular-sanitize.js',
+                        'bower_components/angular-resource/angular-resource.js',
+                        'bower_components/angular-ui-router/release/angular-ui-router.js'
+                    ]
+                }
+            }
+        },
+
         exec: {
             linux: {
                 command: './build/Freeswitch\\ Desktop/linux64/Freeswitch\\ Desktop',
@@ -35,6 +56,9 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-node-webkit-builder');
     grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.registerTask('bundle', ['newer:uglify']);
     grunt.registerTask('build', ['nodewebkit']);
     grunt.registerTask('run', ['build', 'exec:mac']);
     grunt.registerTask('run:linux', ['build', 'exec:linux']);
