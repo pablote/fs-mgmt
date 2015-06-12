5dda4159ca534aa20405877329c2fa527c894498
Pablo Compagni <pablocompagni@gmail.com>
2015-06-12 17:17:41 -0300

    moved settings to main app view/controller

--- src/js/controllers/conferences.js
+++ src/js/controllers/conferences.js
@@ -6,69 +6,16 @@
         'fsmgmt.services.GrowlService',
         'fsmgmt.services.freeswitch.FreeswitchRouter',
         'fsmgmt.directives.ngConfirmClick',
-        'fsmgmt.directives.ngMomentAgo',
         'fsmgmt.directives.ngPopover'
     ]);
-
-    var consts = {
-        StorageKeys: {
-            FreeswitchServerList: 'settings-server-list',
-            FreeswitchUsername: 'settings-username-v2',
-            FreeswitchPassword: 'settings-password-v2',
-            AutoRefreshInterval: 'settings-autorefresh-interval-v2'
-        }
-    };
 
     module.controller('ConferencesController', ['$scope', '$interval', 'localStorage', 'growl', 'freeswitch',
         function ($scope, $interval, localStorage, growl, freeswitch) {
             var gui = require('nw.gui');
             var u = require('underscore');
 
-            window.moment = require('moment');
-            window.moment.fn.fromNowOrNow = function (a) {
-                if (Math.abs(moment().diff(this)) < 3000) {
-                    return 'just now';
-                }
-                return this.fromNow(a);
-            };
-
             // default values
-            $scope.isSettingsVisible = true;
-            $scope.isAutoRefreshEnabled = false;
-            $scope.settings = {};
-            $scope.autoRefresh = null; //interval object
             $scope.servers = [];
-
-            localStorage.get(consts.StorageKeys.FreeswitchServerList).then(function(value) {
-                if (value) {
-                    // compatibility, if no enabled property in object, then default it to true
-                    u.each(value, function (server) {
-                        if (!server.hasOwnProperty('enabled')) {
-                            server.enabled = true;
-                        }
-                    });
-
-                    $scope.settings.serverList = value;
-                }
-                else {
-                    $scope.settings.serverList = [];
-                }
-            });
-
-            localStorage.get(consts.StorageKeys.FreeswitchUsername).then(function(value) {
-                if (value) $scope.settings.username = value;
-            });
-
-            localStorage.get(consts.StorageKeys.FreeswitchPassword).then(function(value) {
-                if (value) $scope.settings.password = value;
-            });
-
-            localStorage.get(consts.StorageKeys.AutoRefreshInterval).then(function(value) {
-                if (value)
-                    $scope.settings.autoRefreshInterval = value;
-                else
-                    $scope.settings.autoRefreshInterval = 5;
-            });
 
             // methods
             $scope.refresh = function () {
@@ -143,61 +90,6 @@
                     });
             };
 
-            $scope.toggleSettings = function () {
-                $scope.isSettingsVisible = !$scope.isSettingsVisible;
-            };
-
-            $scope.toggleAutoRefresh = function () {
-                $scope.isAutoRefreshEnabled = !$scope.isAutoRefreshEnabled;
-
-                if ($scope.isAutoRefreshEnabled) {
-                    $scope.lastRefresh = null;
-                    $scope.autoRefresh = $interval(function() {
-                        $scope.refresh();
-                    }, $scope.settings.autoRefreshInterval * 1000);
-                } else {
-                    $interval.cancel($scope.autoRefresh);
-                    delete $scope.autoRefresh;
-                }
-            };
-
-            $scope.addServer = function () {
-                var host = prompt('Server host:');
-
-                if (host) {
-                    var existingServers = u.where($scope.settings.serverList, { name: host });
-
-                    if (existingServers.length === 0) {
-                        $scope.settings.serverList.push({
-                            name: host,
-                            host: host,
-                        });
-                    } else {
-                        alert ('There\'s already a server by that name');
-                    }
-                }
-            };
-
-            $scope.removeServer = function (server) {
-                $scope.settings.serverList = u.without($scope.settings.serverList, server);
-            };
-
-            // watches
-            $scope.$watch("settings.serverList", function (newValue, oldValue) {
-                localStorage.set(consts.StorageKeys.FreeswitchServerList, newValue);
-            }, true);
-
-            $scope.$watch("settings.username", function (newValue, oldValue) {
-                localStorage.set(consts.StorageKeys.FreeswitchUsername, newValue);
-            });
-
-            $scope.$watch("settings.password", function (newValue, oldValue) {
-                localStorage.set(consts.StorageKeys.FreeswitchPassword, newValue);
-            });
-
-            $scope.$watch("settings.autoRefreshInterval", function (newValue, oldValue) {
-                localStorage.set(consts.StorageKeys.AutoRefreshInterval, newValue);
-            })
         }
     ]);
 }());
--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -3,13 +3,69 @@
     'use strict';
     var module = angular.module('fsmgmt.controllers.main', [
         'ui.router',
+        'fsmgmt.services.LocalStorageService',
         'fsmgmt.controllers.conferences',
-        'fsmgmt.directives.ngModalClose'
+        'fsmgmt.directives.ngModalClose',
+        'fsmgmt.directives.ngMomentAgo'
     ]);
 
-    module.controller('MainController', ['$scope',
-        function ($scope) {
+    var consts = {
+        StorageKeys: {
+            FreeswitchServerList: 'settings-server-list',
+            FreeswitchUsername: 'settings-username-v2',
+            FreeswitchPassword: 'settings-password-v2',
+            AutoRefreshInterval: 'settings-autorefresh-interval-v2'
+        }
+    };
+
+    module.controller('MainController', ['$scope', '$interval', 'localStorage',
+        function ($scope, $interval, localStorage) {
+            var u = require('underscore');
+            window.moment = require('moment');
+            window.moment.fn.fromNowOrNow = function (a) {
+                if (Math.abs(moment().diff(this)) < 3000) {
+                    return 'just now';
+                }
+                return this.fromNow(a);
+            };
+
+            // default values
+            $scope.isSettingsVisible = true;
+            $scope.settings = {};
+            $scope.isAutoRefreshEnabled = false;
+            $scope.autoRefresh = null; //interval object
             $scope.messageDialog = {};
+
+            localStorage.get(consts.StorageKeys.FreeswitchServerList).then(function(value) {
+                if (value) {
+                    // compatibility, if no enabled property in object, then default it to true
+                    u.each(value, function (server) {
+                        if (!server.hasOwnProperty('enabled')) {
+                            server.enabled = true;
+                        }
+                    });
+
+                    $scope.settings.serverList = value;
+                }
+                else {
+                    $scope.settings.serverList = [];
+                }
+            });
+
+            localStorage.get(consts.StorageKeys.FreeswitchUsername).then(function(value) {
+                if (value) $scope.settings.username = value;
+            });
+
+            localStorage.get(consts.StorageKeys.FreeswitchPassword).then(function(value) {
+                if (value) $scope.settings.password = value;
+            });
+
+            localStorage.get(consts.StorageKeys.AutoRefreshInterval).then(function(value) {
+                if (value)
+                    $scope.settings.autoRefreshInterval = value;
+                else
+                    $scope.settings.autoRefreshInterval = 5;
+            });
 
             // mac menus
             if (process.platform === 'darwin') {
@@ -25,6 +81,46 @@
                 win.menu = nativeMenuBar;
             }
 
+            // settings methods
+            $scope.toggleSettings = function () {
+                $scope.isSettingsVisible = !$scope.isSettingsVisible;
+            };
+
+            $scope.toggleAutoRefresh = function () {
+                $scope.isAutoRefreshEnabled = !$scope.isAutoRefreshEnabled;
+
+                if ($scope.isAutoRefreshEnabled) {
+                    $scope.lastRefresh = null;
+                    $scope.autoRefresh = $interval(function() {
+                        $scope.refresh();
+                    }, $scope.settings.autoRefreshInterval * 1000);
+                } else {
+                    $interval.cancel($scope.autoRefresh);
+                    delete $scope.autoRefresh;
+                }
+            };
+
+            $scope.addServer = function () {
+                var host = prompt('Server host:');
+
+                if (host) {
+                    var existingServers = u.where($scope.settings.serverList, { name: host });
+
+                    if (existingServers.length === 0) {
+                        $scope.settings.serverList.push({
+                            name: host,
+                            host: host,
+                        });
+                    } else {
+                        alert ('There\'s already a server by that name');
+                    }
+                }
+            };
+
+            $scope.removeServer = function (server) {
+                $scope.settings.serverList = u.without($scope.settings.serverList, server);
+            };
+
             // modal dialog methods
             $scope.showModal = function(options) {
                 $scope.messageDialog.title = options.title || '';
@@ -38,6 +134,23 @@
             $scope.onModalClose = function () {
                 $scope.messageDialog = {};
             };
+
+            // watches
+            $scope.$watch("settings.serverList", function (newValue, oldValue) {
+                localStorage.set(consts.StorageKeys.FreeswitchServerList, newValue);
+            }, true);
+
+            $scope.$watch("settings.username", function (newValue, oldValue) {
+                localStorage.set(consts.StorageKeys.FreeswitchUsername, newValue);
+            });
+
+            $scope.$watch("settings.password", function (newValue, oldValue) {
+                localStorage.set(consts.StorageKeys.FreeswitchPassword, newValue);
+            });
+
+            $scope.$watch("settings.autoRefreshInterval", function (newValue, oldValue) {
+                localStorage.set(consts.StorageKeys.AutoRefreshInterval, newValue);
+            })
         }
     ]);
 
@@ -45,6 +158,6 @@
         $urlRouterProvider.otherwise("/conferences");
 
         $stateProvider
-            .state('conferences', { url: "/conferences", controller: 'ConferencesController', templateUrl: "conferences/main.html" })
+            .state('conferences', { url: "/conferences", controller: 'ConferencesController', templateUrl: "conferences/main.html" });
     }]);
 }());
